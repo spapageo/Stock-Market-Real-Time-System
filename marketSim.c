a4a1e82fec43a68808b9266602e12379434b38d7
Spyros Papageorgiou <spyros.papageorgiou@gmail.com>
2011-05-30 00:05:01 +0300

    Moved definitions to separate header

--- marketSim.c
+++ marketSim.c
@@ -13,43 +13,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
-
-#define QUEUESIZE 5000
-
-typedef struct {
-  long id,oldid;
-  long timestamp;
-  int vol;
-  int price1, price2;
-  char action, type;
-} order;
-
-void *Prod (void *q);
-void *Cons (void *q);
-
-order makeOrder();
-inline long getTimestamp();
-void dispOrder(order ord);
-
-int currentPriceX10 = 1000;
-
-struct timeval startwtime, endwtime;
-
-pthread_mutex_t mut;
-
-typedef struct {
-  order item[QUEUESIZE];
-  long head, tail;
-  int full, empty;
-  pthread_mutex_t *mut;
-  pthread_cond_t *notFull, *notEmpty;
-} queue;
-
-queue *queueInit (void);
-void queueDelete (queue *q);
-void queueAdd (queue *q, order ord);
-void queueDel (queue *q, order *ord);
-
+#include "marketSim.h"
 
 // ****************************************************************
 int main() {
